b6c36ae6b9ee0738f79d1cb15446bca96e98c942
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2025-12-15 21:25:46 +0530

    code setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+const Page = () => {
+  return (
+    <div>
+      <Button variant='destructive'>Click Me!</Button>
+    </div>
+  );
+};
+export default Page;
