f36d96c1c7f3f63a7d5b21eedd00cd615e1f9ac9
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2025-12-25 15:05:36 +0530

    project entity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,27 +7,32 @@ import { useState } from 'react';
 import { useTRPC } from '@/trpc/client';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { useRouter } from 'next/navigation';
 
 const Page = () => {
+  const router = useRouter();
   const [value, setValue] = useState('');
   const trpc = useTRPC();
-  const { data: messages } = useQuery(trpc.messages.getMany.queryOptions());
-  const createMessage = useMutation(
-    trpc.messages.create.mutationOptions({
-      onSuccess: () => {
-        toast.success('Message created');
+  const createProject = useMutation(
+    trpc.projects.create.mutationOptions({
+      onSuccess: (data) => {
+        // toast.success('Project created');
+        router.push(`/projects/${data.id}`);
+      },
+      onError: (error) => {
+        toast.error(error.message);
       },
     }),
   );
 
   return (
-    <div className='p-2 max-w-7xl'>
-      <Input value={value} onChange={(e) => setValue(e.target.value)} />
-      <Button disabled={createMessage.isPending} onClick={() => createMessage.mutate({ value: value })}>
-        Invoke Background Job
-      </Button>
-      <br />
-      {JSON.stringify(messages, null, 2)}
+    <div className='h-screen w-screen flex items-center justify-center'>
+      <div className='max-w-7xl mx-auto flex flex-col items-center justify-center gap-y-4'>
+        <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        <Button disabled={createProject.isPending} onClick={() => createProject.mutate({ value: value })}>
+          Submit
+        </Button>
+      </div>
     </div>
   );
 };
